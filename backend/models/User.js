5bbbab12c9e0ad52dd204a8e7fa59859316d6b2c
Tanya <tanyayadavv5@gmail.com>
2025-04-14 17:37:10 -0700

    User Model: mongoose schema

---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, maxlength: 100 },
+  email: { type: String, required: true, unique: true, maxlength: 100 },
+  city: { type: String, maxlength: 100 },
+  state: { type: String, maxlength: 100 },
+  country: { type: String, maxlength: 100 },
+  phoneNumber: { type: String, maxlength: 15 },
+  dob: { type: Date },
+  password: { type: String, required: true }
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
