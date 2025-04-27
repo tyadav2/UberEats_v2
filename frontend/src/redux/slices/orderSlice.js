f3f94b7926335bd707b3ff45b2c4f6661ed99c75
Tanya <tanyayadavv5@gmail.com>
2025-04-26 18:07:06 -0700

    Bugfix: Add to cart bug

--- frontend/src/redux/slices/orderSlice.js
+++ frontend/src/redux/slices/orderSlice.js
@@ -9,13 +9,33 @@ const orderSlice = createSlice({
   name: 'order',
   initialState,
   reducers: {
-    addToCart(state, action) {
-      state.cartItems.push(action.payload);
-    },
-    removeFromCart(state, action) {
-      state.cartItems = state.cartItems.filter(item => item.id !== action.payload.id);
-    },
-    clearCart(state) {
+    
+addToCart: (state, action) => {
+  const newItem = action.payload;
+  const existingItemIndex = state.cartItems.findIndex(item => item._id === newItem._id);
+  
+  if (existingItemIndex !== -1) {
+    state.cartItems[existingItemIndex].quantity += 1;
+  } else {
+    state.cartItems.push({ ...newItem, quantity: 1 });
+  }
+},
+
+removeFromCart: (state, action) => {
+  const { id } = action.payload;
+  const existingItemIndex = state.cartItems.findIndex(item => item._id === id);
+  
+  if (existingItemIndex !== -1) {
+    const existingItem = state.cartItems[existingItemIndex];
+    
+    if (existingItem.quantity === 1) {
+      state.cartItems = state.cartItems.filter(item => item._id !== id);
+    } else {
+      state.cartItems[existingItemIndex].quantity -= 1;
+    }
+  }
+},
+      clearCart(state) {
       state.cartItems = [];
     },
     setOrderStatus(state, action) {
